47119b70068fb0d4d500c3fab2a0d5c102d35c46
Pratyush Karn <pratyush.dainsta@gmail.com>
2025-07-01 02:04:00 +0530

    created ping api

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const app = express();
+
+
+const port = 4500;
+
+
+app.get("/ping", (req, res) => res.status(200).json('Pong'));
+
+app.listen(port, () => {
+    console.log(`Server running at port  ${port}`);
+});
