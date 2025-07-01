728318f84ef5403b078c121b405765e03485981c
Pratyush Karn <pratyush.dainsta@gmail.com>
2025-07-02 01:17:14 +0530

    add test api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 4500;
 
 
 app.get("/ping", (req, res) => res.status(200).json('Pong'));
+app.get("/test", (req, res) => res.status(200).json('success'));
 
 app.listen(port, () => {
     console.log(`Server running at port  ${port}`);
